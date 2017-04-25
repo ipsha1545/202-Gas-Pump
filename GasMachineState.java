ecb62c4290a912c0012b8473ccb57f8072f09fba
Ipsha Mohanty <ipsha1545@gmail.com>
2017-04-25 10:20:15 -0700

    Committing changes
    Committing changes

--- GasMachineState.java
+++ GasMachineState.java
@@ -1,30 +1,18 @@
-/**
- * Write a description of class GasMachineState here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+
 public class GasMachineState extends IGasMachineState 
 {
-    // instance variables - replace the example below with your own
+   
     private int x;
 
-    /**
-     * Constructor for objects of class GasMachineState
-     */
+    
     public GasMachineState()
     {
     }
 
-    /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
-     */
+   
     public int sampleMethod(int y)
     {
-        // put your code here
+       
         return x + y;
     }
 }
