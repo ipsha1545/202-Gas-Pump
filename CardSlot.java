fe374010dc25ee907cc1f1bcb2b74d86ebbf3977
Ipsha Mohanty <ipsha1545@gmail.com>
2017-05-04 02:55:14 -0700

    Modifying CardSlot class
    Modifying CardSlot class for displaying error message when a duplicate card is inserted

--- CardSlot.java
+++ CardSlot.java
@@ -6,7 +6,8 @@ public class CardSlot extends Actor implements IComponent
     public void act() 
     {       
       MouseInfo minfo=Greenfoot.getMouseInfo();
-      OriginalCard oCard = (OriginalCard) getOneIntersectingObject(OriginalCard.class);  
+      OriginalCard oCard = (OriginalCard) getOneIntersectingObject(OriginalCard.class);
+      DuplicateCard dCard = (DuplicateCard) getOneIntersectingObject(DuplicateCard.class);
 
       GasPumpWorld gpw = (GasPumpWorld) getWorld(); 
         
@@ -16,6 +17,15 @@ public class CardSlot extends Actor implements IComponent
                 {                          
                     getWorld().removeObject(oCard);
                     Greenfoot.playSound("swipe.wav");                   
+                }  
+             }
+             
+             if(dCard != null){
+                if(minfo.getX()>700 && minfo.getX()<800 && minfo.getY()>125 && minfo.getY()<200)
+                {                          
+                    getWorld().removeObject(dCard);
+                    getWorld().addObject(new displaymessage("Sorry,the card entered is invalid!!!! Please insert a valid card"),350,10);
+                                      
                 }  
              }
         //}
